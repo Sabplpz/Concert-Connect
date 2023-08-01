0c59ca68e32c2836be7f7cd56c8311fe2e7cdfdd
dionkb <dionkbaskara@gmail.com>
2023-07-31 20:41:53 -0400

    added better placeholder links, logo image, placeholder profile img

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Profile from '../assets/placeholder-image.png';
+import Logo from '../assets/ConcertConnectLogo.png';
 // import { Link } from 'react-router-dom';
 // import Auth from '../utils/auth';
 
@@ -6,7 +8,9 @@ const Header = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-xl" href="/">daisyUI</a>
+                <a href="/">
+                    <img src={Logo} alt="Concert-Connect logo" style={{width: "200px", height: "200px"}}/>
+                </a>
             </div>
             <div className="flex-none gap-2">
                 <div className="form-control">
@@ -15,18 +19,15 @@ const Header = () => {
                 <div className="dropdown dropdown-end">
                 <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                     <div className="w-10 rounded-full">
-                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="profile"/>
+                        <img src={Profile} alt="profile" style={{width: "200px", height: "200px"}}/>
                     </div>
                 </label>
                 <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                    <li>
-                        <a className="justify-between" href="/">
-                            Profile
-                            <span className="badge">New</span>
-                        </a>
-                    </li>
-                    <li><a href="/">Settings</a></li>
-                    <li><a href="/">Logout</a></li>
+                    <li><a href="/Home">Home</a></li>
+                    <li><a href="/Bands">Bands</a></li>
+                    <li><a href="/Concerts">Concerts</a></li>
+                    <li><a href="/Venues">Venues</a></li>
+                    <li><a href="/Profile">Profile</a></li>
                 </ul>
                 </div>
             </div>
