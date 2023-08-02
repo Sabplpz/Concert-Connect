0bb2b2eed103e017bd682f33c00b356f6b4f94f8
Sabplpz <sabrinalpz1994@gmail.com>
2023-08-01 20:35:38 -0400

    adding typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,25 +1,55 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type User {
-    _id: ID!
-    username: String!
-    email: String
-  }
+type User {
+  _id: ID!
+  firstName: String!
+  lastName: String!
+  username: String!
+  email: String!
+  password: String!
+  concerts: [Concert]
+  artists: [Artist]
+  venues: [Venue]
+  reviews: [Review]
+}
 
-  type Auth {
-    token: ID!
-    user: User
-  }
+type Concert {
+  concertName: String!
+  city: String!
+  date: String!
+  artist: [Artist]
+  venue: [Venue]
+  genre: String
+}
 
-  type Query {
-    me: User
-  }
+type Artist {
+  artistName: String!
+  concerts: [Concert]
+  genre: String
+}
 
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-  }
+type Review {
+  type: String!
+  name: String!
+  starRating: Int!
+  text: String
+  userId: User
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Query {
+  me: User
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(fisrtName: String!, lastName: String!, username: String!, email: String!, password: String!): Auth
+}
 `;
 
 module.exports = typeDefs;
