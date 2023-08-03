794530b15861d8100da4ae1ce86cb0459a3601e7
Mia Reid <miareid@Mias-iMac.local>
2023-08-03 18:57:36 -0400

    landing page v1

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -19,67 +19,74 @@ export default function Landing() {
       </div>
     {/* body section */}
     <div className="container mx-auto">  
-    {/* columns */}
-      <div className="columns-2 py-24">
-        <img classname="w-full aspect-square" src={placeholder}/>
-        <div className="text-center text-neutral-content">
-          <h2 className="mb-5 text-2xl font-bold">Add All of the Concerts You've Been To</h2>
-          <p>Easily add concerts, search for your favorite bands, and view your selected favorites.</p>
-        </div>
+
+      <div className="card lg:card-side bg-base-100 shadow-xl ">
+        <figure><img src={placeholder}/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Add All of the Concerts You've Been To</h2>
+            <p>Easily add concerts, search for your favorite artists, and view your selected favorites.</p>
+              <div className="card-actions justify-end">
+              </div>
+          </div>
       </div>
-      <div className="columns-2">
-        <div className="text-center text-neutral-content">
-          <h2 className="mb-5 text-2xl font-bold">Follow Your Friends and Make Plans</h2>
-          <p>View who your friends are planning on seeing, where the concert will be, and when. You can also view their favorite bands, and top genres.</p>
-        </div>
-        <img classname="w-full aspect-square h-24" src={placeholder}/>
+
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <figure><img src={placeholder}/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Follow Your Friends and Make Plans</h2>
+            <p>View who your friends are planning on seeing, where the concert will be, and when. You can also view their favorite artists and top genres.</p>
+          </div>
       </div>
-      <div className="columns-2 py-24">
-        <img src={placeholder}/>
-        <div className="text-center text-neutral-content">
-          <h2 className="mb-5 text-2xl font-bold">Look Up Venues &amp; Scope Out Your Spot</h2>
-          <p>See which artists have played at your favorite venues, long before they were famous.</p>
-        </div>
+    
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <figure><img src={placeholder}/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Look Up Venues &amp; Scope Out Your Spot</h2>
+            <p>See which artists have played at your favorite venues, long before they were famous.</p>
+          </div>
       </div>
-      <h2 className="mb-20 text-2xl font-bold text-center">Recently Added Concerts</h2>
-            <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>Date</th>
-        <th>Concert</th>
-        <th>Venue</th>
-        <th>Location</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      {/* row 2 */}
-      <tr>
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      {/* row 3 */}
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
+      
+      <h2 className="mb-20 mt-20 text-2xl font-bold text-center">Recently Added Concerts</h2>
+      <div className="overflow-x-auto mb-20">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Concert</th>
+              <th>Venue</th>
+              <th>Location</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
     </tbody>
   </table>
 </div>
-<button className="btn btn-outline btn-primary">Sign Up</button>
-<button className="btn btn-outline btn-secondary">View More Concerts</button>
+<div className="grid grid-cols-2 gap-2 mb-20">
+  <button className="btn btn-outline btn-primary">Sign Up</button>
+  <button className="btn btn-outline btn-secondary">View More Concerts</button>
+</div>
         </div>
     </div>
   )
