411bf9a7a306d0bdad35fb21bf798aa426afb515
Mia Reid <miareid@Mias-iMac.local>
2023-08-03 22:03:35 -0400

    landing page styling v2

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -19,16 +19,60 @@ export default function Landing() {
       </div>
     {/* body section */}
     <div className="container mx-auto">  
-
-      <div className="card lg:card-side bg-base-100 shadow-xl ">
-        <figure><img src={placeholder}/></figure>
-          <div className="card-body">
-            <h2 className="card-title">Add All of the Concerts You've Been To</h2>
-            <p>Easily add concerts, search for your favorite artists, and view your selected favorites.</p>
-              <div className="card-actions justify-end">
-              </div>
+    {/* row 1 */}
+     <section>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+          <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+            <img
+              src={placeholder}
+              className="absolute inset-0 h-full w-full object-cover"/>
           </div>
+          <div className="lg:py-24"> 
+            <h2 className="text-3xl font-bold sm:text-2xl">Add All of the Concerts You've Been To</h2>
+            <p className="mt-4 text-gray-600">Easily add concerts, search for your favorite artists, and view your selected favorites.</p>
+            <a href="#" className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400">Get Started Today</a>
+          </div>
+        </div>
       </div>
+    </section>
+
+    {/* row 2 */}
+
+    <section>
+  <div
+    class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
+  >
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div
+        class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full"
+      >
+        <img
+          alt="Party"
+          src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          class="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div class="lg:py-24">
+        <h2 class="text-3xl font-bold sm:text-4xl">Grow your audience</h2>
+
+        <p class="mt-4 text-gray-600">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic
+          atque tenetur quis eius quos ea neque sunt, accusantium soluta minus
+          veniam tempora deserunt? Molestiae eius quidem quam repellat.
+        </p>
+
+        <a
+          href="#"
+          class="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+        >
+          Get Started Today
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
 
       <div className="card lg:card-side bg-base-100 shadow-xl">
         <figure><img src={placeholder}/></figure>
