a0cb6c2dd87c70ba461a03d6d80329a526cc862e
Sabplpz <sabrinalpz1994@gmail.com>
2023-08-03 20:04:50 -0400

    working modal for login

--- client/src/components/LoginSignUpModal.js
+++ client/src/components/LoginSignUpModal.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER, ADD_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React from "react";
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER, ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+import close from "../assets/close.png";
 
 function LoginSignUpModal({ isOpen, onClose }) {
   const [isLogin, setIsLogin] = useState(true);
   const [formState, setFormState] = useState({
-    firstName: '',
-    lastName: '',
-    username: '',
-    email: '',
-    password: '',
-    verifyPassword: ''
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+    verifyPassword: "",
   });
 
   const [login, { error: loginError }] = useMutation(LOGIN_USER);
@@ -21,7 +23,7 @@ function LoginSignUpModal({ isOpen, onClose }) {
     const { name, value } = event.target;
     setFormState({
       ...formState,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -50,79 +52,94 @@ function LoginSignUpModal({ isOpen, onClose }) {
     }
   };
 
-  return isOpen ? (
-    <div>
-      <h3>{isLogin ? 'Login' : 'Sign Up'}</h3>
+  const handleClose = (e) => {
+    if (e.target.id === "wrapper") onClose();
+  };
 
-      <form onSubmit={handleFormSubmit}>
-        {!isLogin && (
-          <>
-            <input
-              type="text"
-              name="firstName"
-              placeholder="First Name"
-              value={formState.firstName}
-              onChange={handleFormChange}
-            />
+  if (!isOpen) return null;
+  return (
+    <div
+      id="wrapper"
+      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-50"
+      onClick={handleClose}
+    >
+      <div className="w-96 bg-white flex flex-col rounded text-black justify-items-center">
+        <button
+          className="text-grey-300 text-xl place-self-end p-2"
+          onClick={() => onClose()}
+        >
+          <img src={close} alt="X to close modal" className="h-4"></img>
+        </button>
+        <div className="place-self-center p-2">
+          <h3>{isLogin ? "Login" : "Sign Up"}</h3>
+
+          <form onSubmit={handleFormSubmit}>
+            {!isLogin && (
+              <>
+                <input
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
+                  value={formState.firstName}
+                  onChange={handleFormChange}
+                />
+
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  value={formState.lastName}
+                  onChange={handleFormChange}
+                />
+
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={formState.username}
+                  onChange={handleFormChange}
+                />
+
+                <input
+                  type="password"
+                  name="verifyPassword"
+                  placeholder="Verify Password"
+                  value={formState.verifyPassword}
+                  onChange={handleFormChange}
+                />
+              </>
+            )}
 
             <input
-              type="text"
-              name="lastName"
-              placeholder="Last Name"
-              value={formState.lastName}
-              onChange={handleFormChange}
-            />
-
-            <input
-              type="text"
-              name="username"
-              placeholder="Username"
-              value={formState.username}
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formState.email}
               onChange={handleFormChange}
             />
 
             <input
               type="password"
-              name="verifyPassword"
-              placeholder="Verify Password"
-              value={formState.verifyPassword}
+              name="password"
+              placeholder="Password"
+              value={formState.password}
               onChange={handleFormChange}
             />
-          </>
-        )}
 
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formState.email}
-          onChange={handleFormChange}
-        />
+            <button type="submit">{isLogin ? "Log In" : "Sign Up"}</button>
 
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formState.password}
-          onChange={handleFormChange}
-        />
+            {(loginError || signupError) && (
+              <p>{loginError?.message || signupError?.message}</p>
+            )}
+          </form>
+        </div>
 
-        <button type="submit">
-          {isLogin ? 'Log In' : 'Sign Up'}
+        <button type="button" onClick={() => setIsLogin(!isLogin)}>
+          Switch to {isLogin ? "Sign Up" : "Login"}
         </button>
-
-        {(loginError || signupError) && (
-          <p>
-            {loginError?.message || signupError?.message}
-          </p>
-        )}
-      </form>
-
-      <button type="button" onClick={() => setIsLogin(!isLogin)}>
-        Switch to {isLogin ? 'Sign Up' : 'Login'}
-      </button>
+      </div>
     </div>
-  ) : null;
+  );
 }
 
 export default LoginSignUpModal;
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,37 +1,78 @@
-import React from 'react';
-import Profile from '../assets/placeholder-image.png';
-import Logo from '../assets/ConcertConnectLogo.png';
-import Auth from '../utils/auth';
+import React from "react";
+import { useState } from "react";
+import Profile from "../assets/placeholder-image.png";
+import Logo from "../assets/ConcertConnectLogo.png";
+import Auth from "../utils/auth";
+import LoginSingUpModal from "./LoginSignUpModal";
 
 const Header = () => {
-    return (
-        <div className="navbar bg-base-100">
-            <div className="flex-1">
-                <a href="/">
-                    <img src={Logo} alt="Concert-Connect logo" style={{width: "200px", height: "200px"}}/>
-                </a>
-            </div>
-            <div className="flex-none gap-2">
-                <div className="form-control">
-                <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-                </div>
-                <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                        <img src={Profile} alt="profile" style={{width: "200px", height: "200px"}}/>
-                    </div>
-                </label>
-                <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                    <li><a href="/Home">Home</a></li>
-                    <li><a href="/Artists">Artists</a></li>
-                    <li><a href="/Concerts">Concerts</a></li>
-                    <li><a href="/Venues">Venues</a></li>
-                    <li><a href="/Profile">Profile</a></li>
-                </ul>
-                </div>
-            </div>
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a href="/">
+          <img
+            src={Logo}
+            alt="Concert-Connect logo"
+            style={{ width: "200px", height: "200px" }}
+          />
+        </a>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
         </div>
-    );
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img
+                src={Profile}
+                alt="profile"
+                style={{ width: "200px", height: "200px" }}
+              />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a href="/Home">Home</a>
+            </li>
+            <li>
+              <a href="/Artists">Artists</a>
+            </li>
+            <li>
+              <a href="/Concerts">Concerts</a>
+            </li>
+            <li>
+              <a href="/Venues">Venues</a>
+            </li>
+            <li>
+              <a href="/Profile">Profile</a>
+            </li>
+          </ul>
+        </div>
+        <div className="">
+          <button
+            className="btn btn-outline btn-primary"
+            onClick={() => setShowModal(true)}
+          >
+            LogIn
+          </button>
+          <LoginSingUpModal
+            isOpen={showModal}
+            onClose={() => setShowModal(false)}
+          ></LoginSingUpModal>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
