4f9e6b698b40849a5aa5b0a5a8004136ab177d88
Mia Reid <miareid@Mias-iMac.local>
2023-08-03 20:59:56 -0400

    feeeeeeeeedgit add -Agit add -A

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -163,63 +163,91 @@ export default function Home() {
             {/* end of top venues collapse table */}
           </div>
           <div className="basis 4/6">
-          <div className="flex items-center">
-            <img src={userIcon} style={{width: 50 + 'px',}}/>
-              <div>
-                <strong>Andrew Alfred</strong>
-                <span>Technical advisor</span>
-              </div>
-</div>
-          </div>
 
-        </div>
-        
-
-{/* Logged in user post form */}
-<div className="container mx-auto">
-  <div className="join">
-    <input className="input input-bordered input-primary join-item" type="text" placeholder="Email"/>
-    <button className="btn join-item rounded-r-full">Post</button>
-  </div>
-  {/* End of logged in used post form */}
-  {/* Users friends post */}
-  <div className="card w-96 bg-base-100 shadow-xl">
-  <div className="card-body">
-    <h2 className="card-title">Sabrina Lopez</h2>
-    <p>Just saw Beyonce</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Like</button>
-      <button className="btn btn-secondary">Comment</button>
-    </div>
-  </div>
+          {/* <!-- component --> */}
+          <div className="p-5 mb-4 bg-base-100 rounded-lg border border-base-200 dark:bg-base-100 dark:border-base-200">
+    <ol className="mt-3 divide-y divide-bg-primary dark:divide-bg-primary">
+        <li>
+            <a href="#" className="block items-center p-3 sm:flex hover:bg-neutral-focus dark:hover:bg-neutral-focus">
+                <img className="mr-6 mb-3 w-12 h-12 rounded-full sm:mb-0" src={userIcon} alt="Jese Leos image"/>
+                <div className="text-bg-neutral-content dark:text-bg-neutral-content">
+                    <div className="text-neutral-content font-normal"><span className="font-medium text-secondary dark:text-secondary">Jese Leos</span> is going to see <span className="font-medium text-primary dark:text-primary">Beyonce</span> at <span className="font-medium text-gray-900 dark:text-white"> The Amway Arena</span> on <span className="font-medium text-gray-900 dark:text-white"> 8/30/2023</span></div>
+                    <div className="text-sm font-normal">"I can't wait!!!!"</div>
+                    <span className="inline-flex items-center text-xs font-normal text-gray-500 dark:text-gray-400">
+                        <svg aria-hidden="true" className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clip-rule="evenodd"></path></svg>
+                        Posted 2 mins ago
+                    </span> 
+                    <div className="block items-center mt-1">
+                      <button className="btn btn-outline btn-primary btn-sm mr-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                        Like
+                      </button>
+                      <button className="btn btn-outline btn-secondary btn-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                        Comment
+                      </button>
+                    </div>
+                </div>
+            </a>
+        </li>
+    </ol>
 </div>
-{/* End of friends post */}
-{/* Users friends post */}
-<div className="card w-96 bg-base-100 shadow-xl">
-  <div className="card-body">
-    <h2 className="card-title">Sabrina Lopez</h2>
-    <p>Just saw Beyonce</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Like</button>
-      <button className="btn btn-secondary">Comment</button>
-    </div>
-  </div>
+<div className="p-5 mb-4 bg-base-100 rounded-lg border border-base-200 dark:bg-base-100 dark:border-base-200">
+    <ol className="mt-3 divide-y divide-bg-primary dark:divide-bg-primary">
+        <li>
+            <a href="#" className="block items-center p-3 sm:flex hover:bg-neutral-focus dark:hover:bg-neutral-focus">
+                <img className="mr-6 mb-3 w-12 h-12 rounded-full sm:mb-0" src={userIcon} alt="Jese Leos image"/>
+                <div className="text-bg-neutral-content dark:text-bg-neutral-content">
+                    <div className="text-neutral-content font-normal"><span className="font-medium text-secondary dark:text-secondary">Jese Leos</span> is going to see <span className="font-medium text-primary dark:text-primary">Beyonce</span> at <span className="font-medium text-gray-900 dark:text-white"> The Amway Arena</span> on <span className="font-medium text-gray-900 dark:text-white"> 8/30/2023</span></div>
+                    <div className="text-sm font-normal">"I can't wait!!!!"</div>
+                    <span className="inline-flex items-center text-xs font-normal text-gray-500 dark:text-gray-400">
+                        <svg aria-hidden="true" className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clip-rule="evenodd"></path></svg>
+                        Posted 2 mins ago
+                    </span> 
+                    <div className="block items-center mt-1">
+                      <button className="btn btn-outline btn-primary btn-sm mr-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                        Like
+                      </button>
+                      <button className="btn btn-outline btn-secondary btn-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                        Comment
+                      </button>
+                    </div>
+                </div>
+            </a>
+        </li>
+    </ol>
 </div>
-{/* End of friends post */}
-{/* Users friends post */}
-<div className="card w-96 bg-base-100 shadow-xl">
-  <div className="card-body">
-    <h2 className="card-title">Sabrina Lopez</h2>
-    <p>Just saw Beyonce</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Like</button>
-      <button className="btn btn-secondary">Comment</button>
-    </div>
-  </div>
+<div className="p-5 mb-4 bg-base-100 rounded-lg border border-base-200 dark:bg-base-100 dark:border-base-200">
+    <ol className="mt-3 divide-y divide-bg-primary dark:divide-bg-primary">
+        <li>
+            <a href="#" className="block items-center p-3 sm:flex hover:bg-neutral-focus dark:hover:bg-neutral-focus">
+                <img className="mr-6 mb-3 w-12 h-12 rounded-full sm:mb-0" src={userIcon} alt="Jese Leos image"/>
+                <div className="text-bg-neutral-content dark:text-bg-neutral-content">
+                    <div className="text-neutral-content font-normal"><span className="font-medium text-secondary dark:text-secondary">Jese Leos</span> is going to see <span className="font-medium text-primary dark:text-primary">Beyonce</span> at <span className="font-medium text-gray-900 dark:text-white"> The Amway Arena</span> on <span className="font-medium text-gray-900 dark:text-white"> 8/30/2023</span></div>
+                    <div className="text-sm font-normal">"I can't wait!!!!"</div>
+                    <span className="inline-flex items-center text-xs font-normal text-gray-500 dark:text-gray-400">
+                        <svg aria-hidden="true" className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clip-rule="evenodd"></path></svg>
+                        Posted 2 mins ago
+                    </span> 
+                    <div className="block items-center mt-1">
+                      <button className="btn btn-outline btn-primary btn-sm mr-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                        Like
+                      </button>
+                      <button className="btn btn-outline btn-secondary btn-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                        Comment
+                      </button>
+                    </div>
+                </div>
+            </a>
+        </li>
+    </ol>
 </div>
-{/* End of friends post */}
 </div>
-{/* End of top venues table */}
+</div>
 </div>
 </div>
     )
