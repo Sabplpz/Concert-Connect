199ef4b4d4d6d2f84879b708b0d894244d714a1b
dionkb <dionkbaskara@gmail.com>
2023-08-04 11:09:13 -0400

    Added more mock seeds, test context, and worked on resolvers

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,14 +1,23 @@
 const db = require('../config/connection');
-const { User, Review } = require('../models');
+const { User, Review, Artist, Concert, Venue } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const reviewSeeds = require('./reviewSeeds.json');
+const artistSeeds = require('./artistSeeds.json');
+const concertSeeds = require('./concertSeeds.json');
+const venueSeeds = require('./venueSeeds.json');
 
 db.once('open', async () => {
   try {
     await Review.deleteMany({});
     await User.deleteMany({});
+    await Artist.deleteMany({});
+    await Concert.deleteMany({});
+    await Venue.deleteMany({});
 
     await User.create(userSeeds);
+    await Artist.create(artistSeeds);
+    await Concert.create(concertSeeds);
+    await Venue.create(venueSeeds);
 
     for (let i = 0; i < reviewSeeds.length; i++) {
       const { _id, userId } = await Review.create(reviewSeeds[i]);
